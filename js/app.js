153e58f5471f3ce7922673200f4ff27f53766536
Keith Lewis <keithlewis@trinityComputers.net>
2017-06-09 16:27:25 -0500

    fixing zingchart integration

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $("#internet-speed-link").on("click", function () {
-        OpenFrame($("#internet-speed-link").data("target"))
+        OpenFrame($("#internet-speed-link").data("target"));
     });
     $("#home-link").on("click", function () {
-        GoHome()
+        GoHome();
     });
 });
 
@@ -11,10 +11,10 @@ function OpenFrame(source) {
   $("#dynamic-content").load(source);
   $("#dynamic-content").show();
   $("#main-content").hide();
-};
+}
 
 function GoHome() {
   $("#main-content").show();
   $("#dynamic-content").hide();
   $("#dynamic-content").children().remove();
-};
+}
