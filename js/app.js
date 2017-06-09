14b75c2db321c87e5572901d6830632e3c8553a5
Keith Lewis <keithlewis@trinityComputers.net>
2017-06-09 16:11:58 -0500

    fixed app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
 $(function () {
     $("#internet-speed-link").on("click", OpenFrame($("#internet-speed-link").data("target")));
     $("#home-link").on("click", GoHome());
-}
+};
  
 function OpenFrame(source) {
   $("#dynamic-content").load(source);
   $("#dynamic-content").show();
   $("#main-content").hide();
-}
+};
 
 function GoHome() {
   $("#main-content").show();
   $("#dynamic-content").hide();
   $("#dynamic-content").children().remove();
-}
+};
  );
